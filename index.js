845d9c254398570996352278117920c8b7773324
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-07-21 10:18:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+class Polygon {
+  constructor (sidesarray){
+    this.sides= sidesarray
+  }
+
+  get countSides(){
+    return this.sides.length
+  }
+
+  get perimeter(){
+    return this.sides.reduce(function(memo, i){return memo + i})
+  }
+
+}
+
+class Triangle extends Polygon {
+
+  get isValid(){
+    let a = ((this.sides[0] + this.sides[1]) > this.sides[2])
+    let b = ((this.sides[1] + this.sides[2]) > this.sides[0])
+    let c = ((this.sides[0] + this.sides[2]) > this.sides[1])
+    function isTrue(e) {if (e === true) return true}
+    if ([a,b,c].every(isTrue)){
+      return true
+    }
+    else
+    {return false}
+
+  }
+}
+
+class Square extends Polygon {
+
+  get isValid(){
+    if (this.perimeter/4 === this.sides[0])
+    {return true}
+    else {
+    return false}
+  }
+
+  get area() {
+    return (this.perimeter/4)**2
+  }
+
+}
