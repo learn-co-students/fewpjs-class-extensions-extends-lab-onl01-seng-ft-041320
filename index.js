d779890e043a59d3003e9ccd5a6ca4419e84091f
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-07-30 16:27:14 -0400

    Done.

--- index.js
+++ index.js
@@ -9,14 +9,39 @@ class Polygon {
         return this.array.length
     }
 
+    // get perimeter() {
+    //     let sum = 0
+    //     for (let side of this.array) {
+    //         sum += side
+    //     }
+    //     return sum
+    // }
+
     get perimeter() {
-        let sum = 0
-        for (let side of this.array) {
-            sum += side
-        }
-        return sum
+        return this.array.reduce(function(accumulator, currentValue) {
+            return accumulator + currentValue
+        })
     }
+
+    // get perimeter() {
+    //     let array = this.array
+    //     function myFunc(accumulator, currentValue) {
+    //         return accumulator + currentValue
+    //     }
+    //     return array.reduce(myFunc)
+    // }
+
+    // get perimeter() {
+    //     let array = this.array
+    //     let reducer = (accumulator, currentValue) => accumulator + currentValue
+    //     return array.reduce(reducer)
+    // }
+
 }
+
+//another reduce example:
+// [1, 2, 3, 4]
+// array.reduce((currentTotal, nextNumberInArray) => currentTotal + nextNumberInArray, 5)
 
 class Triangle extends Polygon {
     get isValid(){
